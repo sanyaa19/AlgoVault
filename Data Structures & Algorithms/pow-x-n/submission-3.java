f3832a17dc94b69948bb9819da53a2ef3eab1dfe
sanyaa19 <166216605+sanyaa19@users.noreply.github.com>
2026-05-21 03:09:43 +0530

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(x == 1) return 1.0;
+        if(x == -1) return(n%2==0)? 1.0 : -1.0;
+
+        double result =1;
+        if(n<0){
+            x = (1/x);
+            n = -n;
+        }
+        while(n>0){
+            if(n%2 != 0){
+                result *= x;
+            }
+            x *= x;
+            n >>= 1;      // same as n/=2;
+        }
+        return result;
+    }
+}
