e15593fc0efd15f85aeea7e5eed3d0956f6aee03
sanyaa19 <166216605+sanyaa19@users.noreply.github.com>
2026-05-26 01:11:05 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] arr = new int[26];
+
+        for(char c : s.toCharArray()){
+            arr[c - 'a'] += 1;
+        }
+
+        for(char c : t.toCharArray()){
+            arr[c - 'a'] -= 1;
+        }
+
+        for(int n : arr){
+            if(n != 0) return false;
+        }
+        
+        return true;
+    }
+}
