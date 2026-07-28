667e51028a989dbe4165d0a29ae73c768772cc34
sanyaa19 <166216605+sanyaa19@users.noreply.github.com>
2026-07-28 15:43:07 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.java
@@ -0,0 +1,40 @@
+// Memoization
+// class Solution {
+//     int[] dp;
+//     int house(int[] nums, int idx){
+//         if(idx == 0) return nums[0];
+//         if(idx < 0) return 0;
+
+//         if(dp[idx] != -1) return dp[idx];
+
+//         int pick = nums[idx] + house(nums, idx-2);
+//         int notPick = house(nums, idx-1);
+
+//         return dp[idx] = Math.max(pick, notPick); 
+//     }
+//     public int rob(int[] nums) {
+//         dp = new int[nums.length];
+//         Arrays.fill(dp, -1);
+
+//         return house(nums, nums.length-1);
+//     }
+// }
+
+// Tabulation
+class Solution {
+    public int rob(int[] nums) {
+       int[] dp = new int[nums.length];
+
+       dp[0] = nums[0];
+
+       for(int i = 1; i < nums.length; i++){
+        int pick = nums[i];
+        if(i > 1) pick += dp[i-2];
+        int notPick = dp[i-1];
+
+        dp[i] = Math.max(pick, notPick);
+       }
+
+       return dp[nums.length - 1];
+    }
+}
